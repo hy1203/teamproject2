71384c99450043b3baf4769d687733e5a281bb48
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-02 17:14:38 +0900

    [Modify] diary 라우트 순서 변경

--- routes/diary.ts
+++ routes/diary.ts
@@ -3,9 +3,9 @@ import { Router } from 'express';
 import diary from '@/controller/diary';
 
 const route = Router();
-route.get("/", diary.redirectGets);
-route.get("/:year/:month", diary.gets);
 route.get("/:year/:month/:day", diary.get);
 route.post("/:year/:month/:day", diary.post);
+route.get("/:year/:month", diary.gets);
+route.get("/", diary.redirectGets);
 
 export default route;
