460947c307752c858df0b194b3d32432f58faab7
dae <cys964@gmail.com>
2023-06-02 15:32:59 +0900

    todo 관련 controller, model & 프론트 약간

--- models/todo.ts
+++ models/todo.ts
@@ -1,24 +1,28 @@
 import { DataTypes, Sequelize } from 'sequelize';
 
 export default function todo(sequelize: Sequelize, dataTypes: typeof DataTypes) {
-  return sequelize.define('todo', {
-    id: {
-      type: dataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    date: {
-      type: dataTypes.DATE,
-      allowNull: false,
-    },
-    content: {
-      type: dataTypes.STRING(255),
-      allowNull: false,
-    },
-  }, {
-    tableName: 'todo',
-    freezeTableName: true,
-    timestamps: false,
-  });
+    return sequelize.define(
+        'todo',
+        {
+            id: {
+                type: dataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            date: {
+                type: dataTypes.DATE,
+                allowNull: false,
+            },
+            content: {
+                type: dataTypes.STRING(255),
+                allowNull: false,
+            },
+        },
+        {
+            tableName: 'todo',
+            freezeTableName: true,
+            timestamps: false,
+        }
+    );
 }
--- routes/index.ts
+++ routes/index.ts
@@ -8,9 +8,13 @@ route.get('/', controller.index);
 route.get('/login', controller.loginPage);
 //signup route
 route.get('/signup', controller.signupPage);
+//todo route
+route.get('/todo', controller.createPage);
 
 //post_login route
 route.post('/login', controller.login);
 //post_signup route
 route.post('/signup', controller.signup);
+//post_todo route
+route.post('/todo', controller.createTodo);
 export default route;
