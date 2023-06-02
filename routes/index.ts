31f9dbf8ab26d9d43cfcaa643a9a331bf5aa3341
dae <cys964@gmail.com>
2023-06-02 13:51:56 +0900

    컨트롤러 수정된거 라우터 변경

--- routes/index.ts
+++ routes/index.ts
@@ -7,10 +7,10 @@ route.get('/', controller.index);
 //login route
 route.get('/login', controller.loginPage);
 //signup route
-route.get('/signup', controller.signup);
+route.get('/signup', controller.signupPage);
 
 //post_login route
-route.post('/login', controller.post_login);
+route.post('/login', controller.login);
 //post_signup route
-route.post('/signup', controller.post_signup);
+route.post('/signup', controller.signup);
 export default route;
