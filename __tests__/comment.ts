ac1edcfa1813d756693d2293c766e4e2899c1dc3
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-06 02:01:55 +0900

    [Refac] COMMENT 생성 API 테스트 코드 리팩토링

--- __tests__/comment.ts
+++ __tests__/comment.ts
@@ -1,54 +1,38 @@
-import { login, genIdPw, genPort } from "@/utils/testutil";
+import { getLoginSession, genIdPw, genPort, getFromDB, getAllFromDB, createFromDB } from "@/utils/testutil";
 import db from "@/models";
-import { User, Todo, Comment } from "@/types/models";
+import { Comment } from "@/types/models";
 import setPort from "@/testapp";
 import request from "supertest";
 
 const app = setPort(genPort());
 
 test("create comment", async () => {
-  const todoResult = await db.todo.findOne({
-    order: [["id", "DESC"]],
-  });
-  const todo = todoResult?.toJSON<Todo>();
   // 코멘트를 생성할 임의의 투두 불러오기
+  const todo = await getFromDB(db.todo, {order: [["id", "DESC"]]});
   if (!todo) return; // 투두가 없으면 테스트 실패
-  const { id: todo_id, user_id } = todo;
-  // 투두의 아이디와 유저 아이디를 가져옴
-  const userResult = await db.user.findOne({
-    where: {
-      id: user_id,
-    },
-  });
-  const user = userResult?.toJSON<User>();
-  // 유저 아이디로 유저를 불러옴
+  // 투두에서 필요한 정보 추출
+  const { id: todo_id, year, month, date, user_id } = todo;
+  // 유저 아이디로 유저 정보 불러옴
+  const user = await getFromDB(db.user, {where: {id: user_id}});
   if (!user) return; // 유저가 없으면 테스트 실패
+  // 유저로 로그인을 하고 쿠키(로그인 세션) 추출
   const { username, password } = user;
-  const loginRes = await login(username, password, app);
-  const cookie = loginRes.header["set-cookie"];
-  // 유저로 로그인을 하고 쿠키(로그인 세션)를 가져옴
-  const content = genIdPw().toString();
+  const cookie = await getLoginSession(username, password, app);
   // 임의의 코멘트 내용을 생성
+  const content = genIdPw().toString();
+  // API로 코멘트 생성
   const res = await request(app)
-    .post(`/comment/${todo.id}`)
+    .post(`/todo/${year}/${month}/${date}/${todo_id}`)
     .set("Cookie", cookie)
-    .send({
-      todo_id,
-      content,
-    });
-  // 코멘트 생성
-  const resComment = res.body as Comment;
+    .send({ todo_id, content });
   // 생성된 코멘트를 받아옴(DB에서 ID로 불러와 다시 비교해야 하기 때문에 필요)
-  expect(resComment?.content).toBe(content);
+  const resComment = res.body as Comment;
   // 코멘트 내용이 일치하는지 확인
+  expect(resComment?.content).toBe(content);
+  // 응답받은 코멘트에서 ID 추출
   const { id } = resComment;
-  const commentResult = await db.comment.findOne({
-    where: {
-      id,
-    },
-  });
-  const dbComment = commentResult?.toJSON<Comment>();
   // ID로 생성된 코멘트를 DB에서 불러옴
-  expect(dbComment?.content).toBe(content);
+  const dbComment = await getFromDB(db.comment, { where: { id } });
   // DB에서 불러온 코멘트 내용이 일치하는지 확인
+  expect(dbComment?.content).toBe(content);
 });
