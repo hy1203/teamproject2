37c11d8e5a3848ef44782dc08515cbc71a7323cb
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-06 02:02:48 +0900

    [Create] COMMENT 조회 API 테스트 생성

--- __tests__/comment.ts
+++ __tests__/comment.ts
@@ -36,3 +36,28 @@ test("create comment", async () => {
   // DB에서 불러온 코멘트 내용이 일치하는지 확인
   expect(dbComment?.content).toBe(content);
 });
+
+test("get comment", async () => {
+  // 코멘트를 생성할 임의의 투두 불러오기
+  const todo = await getFromDB(db.todo, {order: [["id", "DESC"]]});
+  if (!todo) return; // 투두가 없으면 테스트 실패
+  // 투두에서 필요한 정보 추출
+  const { id: todo_id, year, month, date, user_id } = todo;
+  // 임의의 코멘트 생성
+  const content = genIdPw().toString();
+  const comment = await createFromDB(db.comment, {todo_id, content});
+  if (!comment) return; // 코멘트가 생성되지 않으면 테스트 실패
+  // 유저 아이디로 유저를 불러옴
+  const user = await getFromDB(db.user, {where: {id: user_id}});
+  if (!user) return; // 유저가 없으면 테스트 실패
+  const { username, password } = user;
+  // 유저로 로그인을 하고 쿠키(로그인 세션)를 가져옴
+  const cookie = await getLoginSession(username, password, app);
+  // API로 코멘트를 불러옴
+  const res = await request(app)
+    .get(`/todo/${year}/${month}/${date}/${todo_id}`)
+    .set("Cookie", cookie)
+  const resComment = res.body as Comment;
+  // 응답받은 코멘트와 생성된 코멘트가 일치하는지 확인
+  expect(resComment?.content).toBe(content);
+});
