4f6cb014c38b13e65a3888994cad350d4e6df2eb
dae <cys964@gmail.com>
2023-06-05 11:38:41 +0900

    todo 조회, 수정, 삭제

--- controller/todo.ts
+++ controller/todo.ts
@@ -1,30 +1,121 @@
-import { Request, Response } from 'express';
-import db from '@/models';
+import { Request, Response } from "express";
+import db from "@/models";
+import { isLogin } from "@/utils";
+
 export default {
-    createPage,
-    createTodo,
+  createPage,
+  createTodo,
+  getTodo,
+  updateTodo,
+  deleteTodo,
 };
 
 //날짜 확인해서 이상한날짜면 돌리고, 또 문제 없으면 post로 넘기기,
 
 type Todo = {
-    id: number;
-    date: string;
-    content: string;
+  id: number;
+  date: string;
+  content: string;
 };
 
 let ToDos: Todo[] = [];
 
 // 페이지 생성
 async function createPage(req: Request, res: Response) {
-    res.render('todo');
+  res.render("todo");
 }
 
 // 투두 생성
 async function createTodo(req: Request, res: Response) {
-    await db.todo.create({
-        date: new Date(),
-        content: req.body.content,
+  await db.todo.create({
+    date: new Date(),
+    content: req.body.content,
+  });
+  res.send({ result: true });
+}
+
+//투두 조회
+async function getTodo(req: Request, res: Response) {
+  try {
+    const { year, month, date } = req.params;
+    const user = isLogin(req, res);
+    if (!user) return;
+    const result = await db.todo.findAll({
+      where: {
+        year,
+        month,
+        date,
+        user_id: user,
+      },
     });
-    res.send({ result: true });
+
+    const todo = result.map((v) => {
+      return v.toJSON();
+    });
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+// 투두 수정
+async function updateTodo(req: Request, res: Response) {
+  try {
+    const { year, month, date } = req.params;
+    const user = isLogin(req, res);
+    if (!user) return;
+
+    // 수정
+    const result = await db.todo.update(
+      {
+        // content 필드를 수정
+        content: req.body.content,
+      },
+      {
+        // year, month, date, user_id가 일치하는 투두를 수정
+        where: {
+          year,
+          month,
+          date,
+          user_id: user,
+        },
+      }
+    );
+
+    if (result[0] === 0) {
+      return res.status(404).json({ message: "Todo가 존재하지 않음." });
+    }
+
+    res.status(200).json({ message: "Todo 수정 완료." });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+//투두 삭제
+async function deleteTodo(req: Request, res: Response) {
+  try {
+    const { year, month, date } = req.params;
+    const user = isLogin(req, res);
+    if (!user) return;
+
+    // 삭제
+    const result = await db.todo.destroy({
+      // year, month, date, user_id가 일치하는 투두를 삭제
+      where: {
+        year,
+        month,
+        date,
+        user_id: user,
+      },
+    });
+
+    if (result === 0) {
+      return res.status(404).json({ message: "Todo가 존재하지 않음." });
+    }
+
+    res.status(200).json({ message: "Todo 삭제 완료." });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
--- routes/index.ts
+++ routes/index.ts
@@ -1,22 +1,25 @@
-import { Router } from 'express';
-import controller from '@/controller';
-import diary from './diary';
+import { Router } from "express";
+import controller from "@/controller";
+import diary from "./diary";
 
 const route = Router();
 //index route
-route.get('/', controller.index);
+route.get("/", controller.index);
 
 //login route
-route.get('/login', controller.loginPage);
-route.post('/login', controller.login);
+route.get("/login", controller.loginPage);
+route.post("/login", controller.login);
 
 //signup route
-route.get('/signup', controller.signupPage);
-route.post('/signup', controller.signup);
+route.get("/signup", controller.signupPage);
+route.post("/signup", controller.signup);
 
 //todo route
-route.get('/todo', controller.createPage);
-route.post('/todo', controller.createTodo);
+route.get("/todo/:year/:month/:day", controller.createPage);
+route.post("/todo/:year/:month/:day", controller.createTodo);
+route.get("/todo/:year/:month/:day/:user_id", controller.getTodo);
+route.put("/todo/:year/:month/:day/:user_id", controller.updateTodo);
+route.delete("/todo/:year/:month/:day/:user_id", controller.deleteTodo);
 
 //diary route
 route.use("/diary", diary);
