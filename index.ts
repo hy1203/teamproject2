bf2669d3de80bcd61119ef4d50eabab5f66095e1
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-05 14:50:44 +0900

    [Modify] 불필요 코드 제거

--- index.ts
+++ index.ts
@@ -1,29 +1,13 @@
-import process from "process";
 import Express from "express";
 import session from "express-session";
 import cookieParser from "cookie-parser";
 import route from "@/routes";
 import db from "@/models";
 
-const env = process.env.NODE_ENV || "development";
 const app = Express();
 const PORT = 8000;
 
-env === "development" &&
-  (async () => {
-    // if db exists, drop it and create new one.
-    let schemas = await db.sequelize.showAllSchemas({});
-    console.log("schemas", schemas);
-    ["comment", "todo", "diary", "user", "emotion"].forEach(async (table) => {
-      await db.sequelize.dropSchema(table, {});
-    });
-    console.log("drop schema");
-    schemas = await db.sequelize.showAllSchemas({});
-    console.log("schemas", schemas);
-    await db.sequelize.sync();
-    schemas = await db.sequelize.showAllSchemas({});
-    console.log("schemas", schemas);
-  })();
+db.sequelize.sync();
 
 app.use(cookieParser());
 app.use(
