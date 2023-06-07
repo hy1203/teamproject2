2ff4bc079d805c65aaca3410ab14d9f8d7a4b64b
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-07 11:09:33 +0900

    [Modify] isLogin JWT 적용 중

--- utils/login.ts
+++ utils/login.ts
@@ -1,7 +1,43 @@
+import process from "process";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import db from "@/models";
+import { getFromDB } from "./getDB";
 
-export default function isLogin(req: Request, res: Response) {
-    const { user } = req.session;
-    if (!user) res.redirect("/");
-    else return user;
+export default async function isLogin(req: Request, res: Response) {
+  let access = (req.headers.authorization || req.cookies.access)?.substring(7);
+  let refresh = (req.headers.refresh || req.cookies.refresh) as string;
+  console.log(access, refresh);
+  if (access && refresh) {
+    const verified = jwt.verify(
+      access,
+      process.env.ACCESS_TOKEN!
+    ) as jwt.JwtPayload;
+    if (verified.authed) return verified.id;
+  }
+  if (!refresh) {
+    res.redirect("/login");
+    return;
+  }
+  try {
+    const { id } = jwt.verify(
+      refresh,
+      process.env.REFRESH_TOKEN!
+    ) as jwt.JwtPayload;
+    const user = await getFromDB(db.user, { where: { id } });
+    if (!user) throw new Error("유저 정보 없음");
+    if (user.refresh !== refresh) {
+      res.redirect("/login");
+      return;
+    }
+    const access = jwt.sign({ user: id }, process.env.ACCESS_TOKEN!, {
+      expiresIn: "1h",
+    });
+    req.headers.authorization = `Bearer ${access}`;
+    return id;
+  } catch (err) {
+    console.log("토큰 갱신 오류", err);
+    res.redirect("/login");
+  }
+  res.redirect("/login");
 }
