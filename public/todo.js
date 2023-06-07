400579aa39ca609a6cad9d50dde6731aad98878c
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-07 14:45:40 +0900

    [Modify] TODO API 변경에 따른 페이지 소스 수정

--- public/todo.js
+++ public/todo.js
@@ -1,5 +1,6 @@
-const apiURL = `/api${window.location.pathname}`;
-console.log(apiURL);
+const apiDateURL = `/api${window.location.pathname}`; // get, post, deleteAll
+const apiIndivURL = (id) => `/api/todo/${id}`; // put, patch, delete
+console.log(apiDateURL);
 const todoForm = document.forms.todoForm;
 const todoList = document.querySelector("ul#todoList");
 const clearBtn = document.getElementById("clear");
@@ -9,16 +10,20 @@ clearBtn.addEventListener("click", clearTodoList);
 initTodo();
 
 async function initTodo() {
-  const todos = await (await fetch(apiURL)).json();
+  // 서버에서 투두리스트를 가져와서 화면에 렌더링
+  const todos = await (await fetch(apiDateURL)).json();
   todos.forEach(({ id, checked, content }) => appendTodo(id, checked, content));
 }
 
 async function removeTodo(e) {
+  // 투두 ID 추출
   const todo = e.target.closest("li");
   const id = Number(todo.id);
   try {
+    // 서버에서 투두 삭제
     const res = await deleteTodo(id);
     if (!res.ok) throw new Error(res.status);
+    // 화면에서 투두 삭제
     todoList.removeChild(todo);
     console.log("투두리스트가 성공적으로 삭제되었습니다.");
   } catch (error) {
@@ -27,8 +32,9 @@ async function removeTodo(e) {
 }
 
 async function deleteTodo(id) {
+  // 서버에서 투두 삭제
   try {
-    const res = await fetch(`${apiURL}/${id}`, { method: "DELETE" });
+    const res = await fetch(apiIndivURL(id), { method: "DELETE" });
     if (!res.ok) throw new Error(res.status);
     return res;
   } catch {
@@ -37,8 +43,14 @@ async function deleteTodo(id) {
 }
 
 async function clearTodoList(e) {
-  const result = await fetch(apiURL, { method: "DELETE" });
-  Array.from(todoList.children).forEach((child) => child.remove());
+  // 서버에서 투두리스트 전체 삭제
+  try {
+    const res = await fetch(apiDateURL, { method: "DELETE" });
+    if (!res.ok) throw new Error(res.status);
+    Array.from(todoList.children).forEach((child) => child.remove());
+  } catch (error) {
+    console.error("투두리스트 전체 삭제 중 오류가 발생했습니다.", error);
+  }
 }
 
 async function addTodo(e) {
@@ -46,10 +58,12 @@ async function addTodo(e) {
   const todo = todoForm.todo.value;
   try {
     if (todo !== "") {
-      const { id, checked } = await postTodo(todo); // 투두리스트를 서버에 저장하고 아이디 값을 받아온다.
+      // 투두리스트를 서버에 저장 후 아이디 값 수신
+      const { id, checked } = await postTodo(todo);
+      // 화면에 투두리스트 추가
       appendTodo(id, checked, todo);
     }
-    todoForm.todo.value = "";
+    todoForm.reset();
   } catch (error) {
     console.error("투두리스트 추가 중 오류가 발생했습니다.", error);
   }
@@ -84,7 +98,7 @@ async function toggleTodo(e) {
   const id = Number(todo.id);
   const checked = e.target.checked;
   try {
-    const res = await fetch(`${apiURL}/${id}`, {
+    const res = await fetch(apiIndivURL(id), {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ checked }),
@@ -98,7 +112,7 @@ async function toggleTodo(e) {
 
 async function postTodo(content) {
   try {
-    const res = await fetch(apiURL, {
+    const res = await fetch(apiDateURL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ content }),
