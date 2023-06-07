8f751b71eab479bf7d68bde166ebb2f21e12389b
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-07 11:35:34 +0900

    [Fix] COMMENT API 분리

--- api/todo.ts
+++ api/todo.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import todo from "@/controller/todo";
+import comment from "@/controller/comment";
 
 const route = Router();
+
+// comment
+route.post("/:year/:month/:day/:todo_id", comment.create);
+route.put("/:year/:month/:day/:todo_id", comment.update);
+route.delete("/:year/:month/:day/:todo_id", comment.destroy);
+
+// todo
 route.post("/:year/:month/:date", todo.post);
 route.get("/:year/:month/:date", todo.get);
 route.put("/:year/:month/:date/:id", todo.put);
--- routes/todo.ts
+++ routes/todo.ts
@@ -1,11 +1,7 @@
 import { Router } from "express";
 import todo from "@/controller/todo";
-import comment from "@/controller/comment";
+
 const route = Router();
 route.get("/:year/:month/:date", todo.createPage);
 
-//투두 comment
-route.post("/:year/:month/:day/:todo_id", comment.create);
-route.put("/:year/:month/:day/:todo_id", comment.update);
-route.delete("/:year/:month/:day/:todo_id", comment.destroy);
 export default route;
