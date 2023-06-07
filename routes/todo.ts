3fbed64971687c1fed94bbda88e145dfef7f6d43
dae <cys964@gmail.com>
2023-06-07 09:49:25 +0900

    todo comment route 설정

--- routes/todo.ts
+++ routes/todo.ts
@@ -4,12 +4,12 @@ import todo from "@/controller/todo";
 const route = Router();
 // route.get("/:year/:month/:date", todo.createPage);
 route.post("/:year/:month/:date", todo.post);
-route.get("/:year/:month/:date", todo.get);
+route.get("/:year/:month/:date/", todo.get);
 route.put("/:year/:month/:date", todo.put);
 route.delete("/:year/:month/:date", todo.destroy);
 
 //투두 comment
-// route.post("/:year/:month/:day/comment", todo.createTodoComment);
-// route.put("/:year/:month/:day/comment", todo.updateTodoComment);
-// route.delete("/:year/:month/:day/comment", todo.deleteTodoComment);
+route.post("/:year/:month/:day/comment", todo.createTodoComment);
+route.put("/:year/:month/:day/comment", todo.updateTodoComment);
+route.delete("/:year/:month/:day/comment", todo.deleteTodoComment);
 export default route;
