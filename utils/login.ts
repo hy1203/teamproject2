73f3ec1f15a850a56b3316012416d5ed0b66659b
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-08 12:11:41 +0900

    [Comment] 주석 추가

--- utils/login.ts
+++ utils/login.ts
@@ -12,15 +12,17 @@ export default async function isLogin(req: Request, res: Response) {
     return id;
   } catch (err) {
     if (err instanceof jwt.TokenExpiredError) {
+      // access 토큰이 만료된 경우에만 refresh 토큰 검증
       console.log("Access 토큰 만료");
     } else {
+      // 그외의 경우 모두 로그인 페이지로 리디렉션
       console.log("Access 토큰 검증 오류", err);
       res.redirect("/login");
       return;
     }
   }
 
-  // access 토큰이 없거나 유효하지 않은 경우에만 refresh 토큰 확인
+  // refresh 토큰 확인
   const refresh = (req.headers.refresh || req.cookies.refresh) as string;
   if (!refresh) {
     res.redirect("/login");
@@ -37,7 +39,7 @@ export default async function isLogin(req: Request, res: Response) {
       // refresh 토큰이 일치하지 않는 경우
       throw new Error("Refresh 토큰 불일치");
     }
-
+    // 새 access 토큰 발급
     const newAccess = jwt.sign({ user: id }, config.ACCESS_TOKEN!, {
       expiresIn: "1h",
     });
@@ -48,6 +50,6 @@ export default async function isLogin(req: Request, res: Response) {
     console.log("Refresh 토큰 검증 오류", err);
   }
 
-  // 로그인 페이지로 리디렉션합니다.
+  // 이외의 경우 로그인 페이지로 리디렉션합니다.
   res.redirect("/login");
 }
