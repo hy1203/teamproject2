b504577fdcb27c61801cbf162f1693dc9ad493d1
dae <cys964@gmail.com>
2023-06-08 11:15:16 +0900

    refresh토큰 access토큰이 만료된경우에 확인하는거로 수정

--- utils/login.ts
+++ utils/login.ts
@@ -1,6 +1,5 @@
-import process from "process";
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import db from "@/models";
 import config from "@/config/token";
 import { getFromDB } from "./getDB";
@@ -9,61 +8,45 @@ export default async function isLogin(req: Request, res: Response) {
   let access = req.headers.authorization || req.cookies.access;
   let refresh = (req.headers.refresh || req.cookies.refresh) as string;
 
-  // access 토큰과 refresh 토큰이 모두 존재하는 경우
-  if (access && refresh) {
+  // access 토큰이 존재하는 경우
+  if (access) {
     const verifiedAccess = jwt.verify(
       access,
       config.ACCESS_TOKEN
-    ) as jwt.JwtPayload;
+    ) as JwtPayload;
 
     // access 토큰이 유효하고 인증된 경우
     if (verifiedAccess.authed) return verifiedAccess.id;
-    const verifiedRefresh = jwt.verify(
-      refresh,
-      config.REFRESH_TOKEN!
-    ) as jwt.JwtPayload;
-    // refresh 토큰이 만료되지 않았고 인증된 경우
-    if (verifiedRefresh.authed) {
-      const { id } = verifiedRefresh;
-      const user = await getFromDB(db.user, { where: { id } });
-      if (!user) throw new Error("유저 정보 없음");
-      if (user.refresh !== refresh) {
-        res.redirect("/login");
-        return;
+
+    // access 토큰이 만료된 경우
+    const decodedAccess = jwt.decode(access) as JwtPayload;
+    if (decodedAccess.exp! * 1000 <= Date.now()) {
+      // refresh 토큰이 존재하는 경우
+      if (refresh) {
+        const verifiedRefresh = jwt.verify(
+          refresh,
+          config.REFRESH_TOKEN!
+        ) as JwtPayload;
+
+        // refresh 토큰이 유효하고 인증된 경우
+        if (verifiedRefresh.authed) {
+          const { id } = verifiedRefresh;
+          const user = await getFromDB(db.user, { where: { id } });
+          if (!user) throw new Error("유저 정보 없음");
+          if (user.refresh !== refresh) {
+            res.redirect("/login");
+            return;
+          }
+          const newAccess = jwt.sign({ user: id }, config.ACCESS_TOKEN!, {
+            expiresIn: "1h",
+          });
+          req.headers.authorization = `Bearer ${newAccess}`;
+          return id;
+        }
       }
-      const newAccess = jwt.sign({ user: id }, config.ACCESS_TOKEN!, {
-        expiresIn: "1h",
-      });
-      req.headers.authorization = `Bearer ${newAccess}`;
-      return id;
     }
   }
 
-  // refresh 토큰이 없는 경우
-  if (!refresh) {
-    res.redirect("/login");
-    return;
-  }
-
-  // refresh 토큰이 만료된 경우
-  try {
-    const { id } = jwt.verify(refresh, config.REFRESH_TOKEN!) as jwt.JwtPayload;
-    const user = await getFromDB(db.user, { where: { id } });
-    if (!user) throw new Error("유저 정보 없음");
-    if (user.refresh !== refresh) {
-      res.redirect("/login");
-      return;
-    }
-    const access = jwt.sign({ user: id }, config.ACCESS_TOKEN!, {
-      expiresIn: "1h",
-    });
-    req.headers.authorization = `Bearer ${access}`;
-    return id;
-  } catch (err) {
-    console.log("토큰 갱신 오류", err);
-    res.redirect("/login");
-  }
-
-  // 기본적으로 로그인 페이지로 리디렉션합니다.
+  // access 토큰이 없거나 유효하지 않은 경우
   res.redirect("/login");
 }
