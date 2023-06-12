302877ce8400952abf234189d2122a2986d3117d
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-12 21:22:25 +0900

    [Fix] COMMENT API 경로 수정

--- api/comment.ts
+++ api/comment.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import comment from "@/controller/comment";
 const route = Router();
 
-route.post("/comment/:todo_id", comment.create);
-route.put("/comment/:todo_id", comment.update);
-route.delete("/comment/:todo_id", comment.destroy);
+route.post("/:todo_id", comment.create);
+route.put("/:todo_id", comment.update);
+route.delete("/:todo_id", comment.destroy);
 
 export default route;
