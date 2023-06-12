625b3f8b1737820a5e1acc113e5f709d46a5e4ce
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-12 15:11:14 +0900

    [Create] ID 중복 확인 API 생성

--- controller/signup.ts
+++ controller/signup.ts
@@ -5,6 +5,7 @@ import isLogin from "@/utils/login";
 export default {
   get,
   post,
+  isDupl,
 };
 
 //회원가입 GET
@@ -16,12 +17,6 @@ async function get(req: Request, res: Response) {
 
 //회원가입 Post
 async function post(req: Request, res: Response) {
-  if (!req.body.password) {
-    // 패스워드 값이 없는 경우 오류 처리
-    console.log("패스워드 값이 없습니다.");
-    res.send({ result: false });
-    return;
-  }
   const { username, password } = req.body;
   const result = await db.user.create({
     username,
@@ -39,4 +34,13 @@ async function post(req: Request, res: Response) {
     console.log("회원가입 실패", err);
     res.send({ result: false });
   }
+}
+
+async function isDupl(req: Request, res: Response) {
+  const { username } = req.query;
+  if (username && typeof username === "string") {
+    const result = await db.user.findOne({ where: { username } });
+    return res.json({ result: result ? true : false });
+  }
+  res.status(404).end();
 }
