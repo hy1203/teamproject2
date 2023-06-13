2cd39a99e28b53f36523749b0cc7ab0bcae2c3e9
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-13 13:07:54 +0900

    [Install] dotenv 설치
    환경 변수의 원활한 이용을 위한 dotenv 라이브러리 설치

--- models/index.ts
+++ models/index.ts
@@ -1,5 +1,6 @@
 import { Sequelize, DataTypes } from "sequelize";
 import process from "process";
+import dotenv from "dotenv";
 import configs from "@/config";
 import User from "./user";
 import Todo from "./todo";
@@ -7,6 +8,7 @@ import Comment from "./comment";
 import Emotion from "./emotion";
 import Diary from "./diary";
 
+dotenv.config();
 const env = process.env.NODE_ENV || "development";
 const { database, username, password, ...config } = configs[env];
 const sequelize = new Sequelize(database, username, password, config);
