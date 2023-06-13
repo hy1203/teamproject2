a6da9a82cb2f7e4565d707685c8127a81c693bd1
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-06-14 02:32:08 +0900

    [Move] todocalendar → todo/calendar 추가 변경

--- api/calendar.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import calendar from "@/controller/calendar";
-const route = Router();
-
-route.get("/calendar/:todo_id", calendar.gets);
-
-export default route;
--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import diary from "./diary";
 import todo from "./todo";
 import upload from "./upload";
-import calendar from "./calendar";
 import emotion from "./emotion";
 import signup from "./signup";
 
@@ -11,7 +10,6 @@ const route = Router();
 route.use("/diary", diary);
 route.use("/todo", todo);
 route.use("/upload", upload);
-route.use("/calendar", calendar);
 route.use("/emotion", emotion);
 route.use("/signup", signup);
 
